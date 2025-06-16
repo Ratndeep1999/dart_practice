256f00861f9874423a164e9963b288a4c8bc30a9
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-16 14:23:47 +0530

    class.dart : examples of class

--- class.dart
+++ class.dart
@@ -3,6 +3,54 @@
 
 // class and object
 
-void main() {
+// Area class
+class Area{
+  double? length = 5.5 ;
+  int? breadth = 5 ;
 
+  double claculateArea(){
+    return length! * breadth! ;
+  }
+}
+
+// Student class
+class Student{
+  String? name ;
+  int? age ;
+  int? grade ;
+
+  void display(){
+    print('Student name : $name');
+    print('Student age : $age');
+    print('Student grade : $grade');
+  }
+}
+
+
+// Book class
+class Book{
+  String? name ;
+  String? author ;
+  double? price ;
+
+  void display(){
+    print('Book name : $name');
+    print('Author name : $author');
+    print('Price : $price');
+  }
+
+
+}
+
+void main() {
+  Area area = Area();
+  Student student = Student();
+
+  print('The area of rectangle is ${area.claculateArea()}');
+  student.display();
+
+  print('\n');
+
+  Book book = Book() ;
+  book.display();
 }
