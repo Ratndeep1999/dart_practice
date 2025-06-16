f2491ab85e642d379c786c5fd78c8a233fc0e152
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-16 15:16:12 +0530

    class.dart : examples of class

--- class.dart
+++ class.dart
@@ -52,6 +52,25 @@ class AreaCircle{
 
 
 
+// class of car
+class Car{
+  String? brand ;
+  int? model ;
+  int? numOfSeats ;
+
+  void printDetails(){
+    print('Brand : $brand');
+    print('Model : $model');
+    print('Number of Setts : $numOfSeats');
+  }
+
+  void carStart(){
+    print("$brand car is Started");
+  }
+}
+
+
+
 void main() {
   Area area = Area();
   Student student = Student();
@@ -61,10 +80,51 @@ void main() {
 
   print('\n');
 
+  // object of book class
   Book book = Book() ;
   book.display();
 
-
+  // object of AreaCircle class
   AreaCircle areaOfCircle = AreaCircle() ;
   print("The area of circle is ${areaOfCircle.calculateArea().toStringAsFixed(2)}");  // 94.98 against 94.985
+
+  print('\n');
+
+  // object of car class
+  Car car = Car() ;
+  car.brand = 'Tesla';
+  car.model = 2025 ;
+  car.numOfSeats = 7 ;
+  car.printDetails();
+  car.carStart();
+
+  print('\n');
+
+  // first object of Camera class
+  Camera camera1 = Camera() ;
+  camera1.name = 'Nikon';
+  camera1.color = 'Dark Black';
+  camera1.megaPixels = 550;
+  camera1.printDetails();
+
+  // second object of Camera class
+  Camera camera2 = Camera() ;
+  camera2.name = 'Sony' ;
+  camera2.color = 'Shinny Black' ;
+  camera2.megaPixels = 1024 ;
+  camera2.printDetails();
+}
+
+
+// challenging task of class Camera
+class Camera{
+  String? name ;
+  String? color ;
+  int? megaPixels ;
+
+  void printDetails(){
+    print('Name : $name');
+    print('Color : $color');
+    print('Number of Pixels : $megaPixels');
+  }
 }
