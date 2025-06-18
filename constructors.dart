fea17d09a28e2826be7fa07dcf53f5f3c61d7b7e
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-18 17:56:49 +0530

    class.dart : examples of class

--- constructors.dart
+++ constructors.dart
@@ -38,6 +38,18 @@ class Person{
 }
 
 
+// e.g of const constructor
+class Customer{
+  final String? name ;
+  final int? age ;
+  final dynamic phone ;
+
+  // const constructor with named and optional parameters
+  const Customer({this.name, this.phone = 'Not linked', this.age }); // here age is optional
+
+}
+
+
 void main(){
 
   // object of patient class
@@ -55,5 +67,30 @@ void main(){
   Person p2 = Person.fromJsonString(jsonString2);
   print("Person 2 name : ${p2.name}");
   print("Person 2 name : ${p2.age}");
+  print('\n');
+
+  // object of Customer
+  const Customer customer = Customer(phone: 8551830830, name: 'Sonali', age: 21 );
+  print('Customer details : ');
+  print('Name : ${customer.name}');
+  print('Age : ${customer.age}');
+  print('Phone : ${customer.phone}');
+  print('\n');
+  // Customer details :
+  // Name : Sonali
+  // Age : 21
+  // Phone : 8551830830
+
+
+  const Customer customer2 = Customer(age: 26, name: 'Priti' );  // here phone is not passed so it takes default value
+  print('Customer details : ');
+  print('Name : ${customer2.name}');
+  print('Age : ${customer2.age}');
+  print('Phone : ${customer2.phone}');
+  // Customer details :
+  // Name : Priti
+  // Age : 26
+  // Phone : Not linked
+
 
 }
