7e349b0d492fc99ef9f865f44d4d7e8924775545
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-22 23:15:15 +0530

    encapsulation.dart : class of Bank, private properties, getters

--- Encapsulation/encapsulation.dart
+++ Encapsulation/encapsulation.dart
@@ -7,4 +7,19 @@ class Bank {
   double _balance = 0.00 ;
 
 
+  // getter to access the values private properties
+  String? get accountHolderName {
+   return this._accountHolderName ;
   }
+
+  int? get accountNumber {
+    return this.accountNumber ;
+  }
+
+  double get balance {
+    return this._balance ;
+  }
+
+
+
+}
