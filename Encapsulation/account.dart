a5b80d7fa0db2b43defab3acadef739a5cfb78f1
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-24 12:30:43 +0530

    encapsulation.dart : class of Bank, private properties, getters, Setter, Withdrawal method, Deposit method,

--- Encapsulation/account.dart
+++ Encapsulation/account.dart
@@ -13,11 +13,7 @@ void main() {
   // Try and catch to fetch validation
   try {
 
-     // myAccount.accountHolderName = null ;
-     myAccount.accountHolderName = 'Ratndeep Chandankhede' ;
 
-     // myAccount.accountNumber = null ;
-     myAccount.accountNumber = 294001000021273 ;
 
     print('Account Holder Name : ${myAccount.accountHolderName}');
     print('Account Number : ${myAccount.accountNumber}');
