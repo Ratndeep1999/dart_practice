9e3681a1a654e29fbff7f550663335681d38b909
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-04 16:17:53 +0530

    inheritance_using_multiple_interface_with_properties.dart : e.g of multiple inheritance by using interface with the properties

--- Abstraction/Interface/inheritance_using_multiple_interface_with_properties.dart
+++ Abstraction/Interface/inheritance_using_multiple_interface_with_properties.dart
@@ -3,18 +3,24 @@
 
 // e.g of multiple inheritance using multiple interfaces with properties
 
-
+// interface 1
 interface class TotalMarks {
 
   void totalMarks() { }
 }
 
+// interface 2
 interface class Percentage {
 
   void percentage() { }
 }
 
+// interface 3
 interface class Grade {
 
   void grade() {}
+}
+
+void main() {
+
 }
