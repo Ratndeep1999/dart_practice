857da3f6adb1b94c7a2c0719fbbf4613640a4beb
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-25 21:54:58 +0530

    Boiled plate add

---
+++ Basics/boiler_plate.dart
@@ -0,0 +1,5 @@
+
+// This is main() function means Entry point of the program
+void main(){  // we can omit void, it automatically inferred automatically
+  print("Hello World"); // Print statement to print string
+}  // {} is the block of the code [Start and End point of section of code]
