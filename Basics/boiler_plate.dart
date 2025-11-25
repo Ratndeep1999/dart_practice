c75fe009cad13968a918bb80c282326878189a33
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-25 21:57:46 +0530

    variable defi.

--- Basics/boiler_plate.dart
+++ Basics/boiler_plate.dart
@@ -3,3 +3,6 @@
 void main(){  // we can omit void, it automatically inferred automatically
   print("Hello World"); // Print statement to print string
 }  // {} is the block of the code [Start and End point of section of code]
+
+// variables : It is the given name to the memory location where we can store data,
+// we can understand it like it is the name of Container, and inside that container we store some data
