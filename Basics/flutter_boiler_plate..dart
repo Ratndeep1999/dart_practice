945e5af1857c9d75fcbf7f1161e9c3995781e119
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-26 18:29:44 +0530

    flutter_boiler_plate..dart : stateless widget boiler plate add

---
+++ Basics/flutter_boiler_plate..dart
@@ -0,0 +1,12 @@
+
+/// StatelessWidget Boiler PLate
+
+// class MyStatelessWidget extends StatelessWidget {
+//   const MyStatelessWidget({super.key});
+//
+//   @override
+//   Widget build(BuilContext context){
+//     return Scaffold();
+//   }
+// }
+
