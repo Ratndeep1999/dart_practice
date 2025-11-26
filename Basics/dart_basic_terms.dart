48e1f57e612028ca6b17b09ef455506cf207d9f9
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-26 18:38:48 +0530

    dart_basic_terms.dart : programming, coding, statement,

--- Basics/dart_basic_terms.dart
+++ Basics/dart_basic_terms.dart
@@ -1,4 +1,7 @@
-
-
 // Programing : process of telling computer that which task has to perform in order to solve the problem.
+
+// Coding : set of instructions that read by computer in binary codes (0 and 1).
+
+// Statement : commands that tell computer to do something,
+// it is a lines of codes which is end with semi colon.
 
