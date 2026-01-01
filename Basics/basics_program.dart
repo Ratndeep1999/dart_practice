a325dec54e2a9539f654eb644a9d4223fbd8bb11
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-01 20:22:01 +0530

    basics_program.dart : basic program add

---
+++ Basics/basics_program.dart
@@ -0,0 +1,6 @@
+// Basic Program
+void main() {
+  String name = "Ratndeep ", surname = "Chandankhede";
+  print(name + surname); // String Concatenation
+  print("My Name is $name and my surname is $surname"); // String Interpolation
+}
--- Basics/boiler_plate.dart
+++
@@ -1,5 +0,0 @@
-
-// this is main entry point of the program that return nothing
-void main() {
-  print("Hello World");  // print statement use to print values
-}  // curly braces defines {} body of the code (block of the code)
