c526183708ba2a8ebbf239ab85678a2b5819eef3
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-01 20:35:25 +0530

    variables.dart : definition, rules, examples

---
+++ Basics/variables.dart
@@ -0,0 +1,17 @@
+/// Variable
+// given name to memory location where we store any kind of value
+
+/// Rules
+// start with letters only.
+// case sensitive.
+// can contain only _ ,$ and numbers.
+// not same as keyword.
+// can not start with numbers
+// space is not allowed
+
+void main() {
+  String name = "Ratndeep"; // here "name" is variable which holds string type of value.
+  int age = 26; // here "age" is variable name 26 is int type of value.
+  double heightCM = 170.2;
+  num tempe = 25, average = 25.6;
+}
